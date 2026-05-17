df7e3dbee6f4bfb126a8232847c6100c391604d8
quiqxiq <quiqxiq@gmail.com>
2026-05-18 02:22:24 +0700

    Remove roslib-mikhmon from tracking

--- roslib-mikhmon/scripts/transaction/doc.go
+++
@@ -1,9 +0,0 @@
-// Package transaction menyediakan format/parse nama /system/script
-// transaksi mikhmon.
-//
-// Format nama:
-//
-//	<date>-|-<time>-|-<user>-|-<price>-|-<ip>-|-<mac>-|-<validity>-|-<profile>-|-<comment>
-//
-// Cross-ref: analisis §3.1 (tabel "Konvensi field pada nama script transaksi").
-package transaction
